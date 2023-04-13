1847507b20776a9d3a7e0d0eac198d6ddafa7010
Fernando Roberto Pereira <fernando.pereira@ifsc.edu.br>
2023-04-12 21:01:29 -0300

    Added AP01.

--- AP01/Ex01/src/Main.java
+++ AP01/Ex01/src/Main.java
@@ -4,12 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Endereco e1 = new Endereco("Expedicionário", "Campo d'Água Verde", 1549);
+		Endereco e1 = new Endereco("Expedicionários", "Campo d'Água Verde", 1549);
 		Endereco e2 = new Endereco("Felipe Schmidt", "Centro", 10);
 
 		Pessoa p1 = new Pessoa("João", e1);
 		p1.exibeEnderecos();
 		Pessoa p2 = new Pessoa("Maria", e2);
+		p2.adicionaEndereco(e1);
 		p2.exibeEnderecos();
 	}
 }
